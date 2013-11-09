2b944b6542b9b8d1dec4818da53f2e8717836d32
Felix Klinge <f.klinge@k15games.de>
2013-11-09 17:31:02 +0100

    #ADDED BlockAllocator as replacement for PageAllocator

---
+++ engine/include/K15_BlockAllocator.h
@@ -0,0 +1,63 @@
+/**
+ * @file K15_BlockAllocator.h
+ * @author  Felix Klinge <f.klinge@k15games.de>
+ * @version 1.0
+ * @date 2013/11/07
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ * 
+ */
+
+#ifndef _K15Engine_Core_BlockAllocator_h_
+#define _K15Engine_Core_BlockAllocator_h_
+
+#ifndef K15_USE_PRECOMPILED_HEADER
+#	include "K15_Prerequisites.h"
+#	include "K15_BaseAllocator.h"
+#endif //K15_USE_PRECOMPILED_HEADER
+
+namespace K15_Engine { namespace Core {
+	class K15_CORE_API BlockAllocator : public BaseAllocator
+	{
+	private:
+		/*********************************************************************************/
+		struct MemoryBlock
+		{
+			bool Used;
+			byte* Memory;
+			uint32 Size;
+			MemoryBlock* Next;
+		};
+		/*********************************************************************************/
+	public:
+		BlockAllocator(uint32 p_Size,const ObjectName& p_Name);
+		BlockAllocator(BaseAllocator* p_Allocator,uint32 p_Size,const ObjectName& p_Name);
+		virtual ~BlockAllocator();
+
+		virtual void clear() OVERRIDE;
+		virtual void* alloc(uint32 p_Size) OVERRIDE;
+		virtual void free(void* p_Pointer) OVERRIDE;
+
+	private:
+		void mergeBlocks();
+		void createBlock(MemoryBlock* p_Successor);
+
+	private:
+		MemoryBlock* m_First;
+	};// end of BlockAllocator class declaration
+}}// end of K15_Engine::Core namespace
+
+#endif //K15Engine_Core_BlockAllocator_h_
---
+++ engine/src/K15_BlockAllocator.cpp
@@ -0,0 +1,156 @@
+/**
+ * @file K15_BlockAllocator.cpp
+ * @author  Felix Klinge <f.klinge@k15games.de>
+ * @version 1.0
+ * @date 2013/11/07
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#include "K15_PrecompiledHeader.h"
+
+#include "K15_BlockAllocator.h"
+
+namespace K15_Engine { namespace Core {
+	/*********************************************************************************/
+	BlockAllocator::BlockAllocator(uint32 p_Size,const ObjectName& p_Name)
+		: BaseAllocator(p_Size,p_Name),
+		  m_First((MemoryBlock*)::malloc(sizeof(MemoryBlock)))
+	{
+		m_First->Memory = m_Memory;
+		m_First->Size = m_MemorySize;
+		m_First->Used = false;
+		m_First->Next = 0;
+		clear();
+	}
+	/*********************************************************************************/
+	BlockAllocator::BlockAllocator(BaseAllocator* p_Allocator,uint32 p_Size,const ObjectName& p_Name)
+		: BaseAllocator(p_Allocator,p_Size,p_Name),
+		  m_First((MemoryBlock*)::malloc(sizeof(MemoryBlock)))
+	{
+		m_First->Memory = m_Memory;
+		m_First->Size = m_MemorySize;
+		m_First->Used = false;
+		m_First->Next = 0;
+		clear();
+	}
+	/*********************************************************************************/
+	BlockAllocator::~BlockAllocator()
+	{
+		clear();
+		::free(m_First);
+	}
+	/*********************************************************************************/
+	void BlockAllocator::clear()
+	{
+		MemoryBlock* currentBlock = m_First->Next;
+		MemoryBlock* nextBlock = 0;
+		while(currentBlock)
+		{
+			nextBlock = currentBlock->Next;
+			::free(currentBlock);
+			currentBlock = nextBlock;
+		}
+	}
+	/*********************************************************************************/
+	void* BlockAllocator::alloc(uint32 p_Size)
+	{
+		MemoryBlock* currentBlock = m_First;
+		bool merged = false;
+		while(true)
+		{
+			if(!currentBlock->Used)
+			{
+				if(currentBlock->Size >= p_Size)
+				{
+					break; //we got our block!
+				}
+			}
+			if(!currentBlock->Next && !merged)
+			{
+				_LogWarning("BlockAllocator \"%s\" has not enough free memory. Trying to merge blocks...",m_Name.c_str());
+				mergeBlocks();
+				merged = true;
+				currentBlock = m_First;
+			}
+			else
+			{
+				currentBlock = currentBlock->Next;
+
+				K15_ASSERT(currentBlock,StringUtil::format("BlockAllocator \"%s\" has not enough free memory to satisfy memory request.",m_Name.c_str()));
+			}
+		}
+
+		byte* memory = currentBlock->Memory;
+		currentBlock->Size = p_Size;
+		currentBlock->Used = true;
+
+		createBlock(currentBlock);
+
+		return memory;
+	}
+	/*********************************************************************************/
+	void BlockAllocator::free(void* p_Pointer)
+	{
+		MemoryBlock* currentBlock = m_First;
+
+		while(currentBlock->Memory != p_Pointer)
+		{
+			currentBlock = currentBlock->Next;
+			
+			K15_ASSERT(currentBlock,StringUtil::format("Address %p does not belong to BlockAllocator \"%s\".",p_Pointer,m_Name.c_str()));
+		}
+
+		m_UsedMemory -= currentBlock->Size;
+		currentBlock->Used = false;
+	}
+	/*********************************************************************************/
+	void BlockAllocator::mergeBlocks()
+	{
+		MemoryBlock* currentBlock = m_First;
+		while(currentBlock && currentBlock->Next)
+		{
+			if(!currentBlock->Used && !currentBlock->Next->Used)
+			{
+				_LogDebug("Merging block %p(size %u) and block %p(size %u) from BlockAllocator \"%s\"...",
+					currentBlock,currentBlock->Size,currentBlock->Next,currentBlock->Next->Size,m_Name.c_str());
+				
+				currentBlock->Size += currentBlock->Next->Size;
+				currentBlock->Next = currentBlock->Next->Next;
+				
+				::free(currentBlock->Next);
+			}
+
+			currentBlock = currentBlock->Next;
+		}
+	}
+	/*********************************************************************************/
+	void BlockAllocator::createBlock(MemoryBlock* p_Successor)
+	{
+		if((ptrdiff_t)(p_Successor->Memory + p_Successor->Size) > (ptrdiff_t)m_MemoryEndAddress)
+		{
+			_LogWarning("BlockAllocator \"%s\" is full.",m_Name.c_str());
+		}
+		else if((ptrdiff_t)(p_Successor->Memory + p_Successor->Size) != (ptrdiff_t)m_MemoryEndAddress)
+		{
+			MemoryBlock* newblock = (MemoryBlock*)::malloc(sizeof(MemoryBlock));
+			newblock->Used = false;
+			newblock->Memory = p_Successor->Memory + p_Successor->Size;
+			newblock->Size = m_MemorySize - (m_UsedMemory + p_Successor->Size);
+			newblock->Next = 0;
+
+			p_Successor->Next = newblock;
+		}
+	}
+	/*********************************************************************************/
+}}// end of K15_Engine::Core namespace
