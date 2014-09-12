1b6084e88c1819613ddb2cf1b5c79fafdc92dc7e
Felix Klinge <f.klinge15@gmail.com>
2014-09-12 02:18:54 +0200

    #UPDATED Archive script for windows

--- scripts/win32/CreateArchive.bat
+++ scripts/win32/CreateArchive.bat
@@ -1,3 +1,3 @@
 mkdir ..\archives\
 
-rar a -r -x*\bin -x*\archives -x*\obj -x*.sdf -x*\ipch "..\archives\%DATE% k15-engine.rar" ..\*.*
+rar a -r -x*\bin -x*\archives -x*\obj -x*.sdf -x*\ipch "..\archives\%DATE% k15-engine.rar" ..\..\*.*
