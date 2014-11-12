6d04ab8e9e0da22b2ffb444de1fb92c60f824541
Felix Klinge <f.klinge15@gmail.com>
2014-11-12 12:21:43 +0100

    #Formatting

--- engine/core/src/K15_BlockAllocator.cpp
+++ engine/core/src/K15_BlockAllocator.cpp
@@ -24,8 +24,8 @@
 
 namespace K15_Engine { namespace Core {
 	/*********************************************************************************/
-	BlockAllocator::BlockAllocator(size_t p_Size,const String& p_Name,BaseAllocator* p_BaseAllocator)
-		: BaseAllocator(p_Size,p_Name,p_BaseAllocator),
+	BlockAllocator::BlockAllocator(size_t p_Size, const String& p_Name, BaseAllocator* p_BaseAllocator)
+		: BaseAllocator(p_Size, p_Name, p_BaseAllocator),
 		  m_First(0),
 		  m_Current(0)
 	{
@@ -44,7 +44,7 @@ namespace K15_Engine { namespace Core {
 	/*********************************************************************************/
 	void* BlockAllocator::alloc(size_t p_Size)
 	{
-    MemoryBlock* block = findBlock_R(m_Current, p_Size);
+		MemoryBlock* block = findBlock_R(m_Current, p_Size);
 
 		if(!block)
 		{
@@ -66,68 +66,68 @@ namespace K15_Engine { namespace Core {
   /*********************************************************************************/
   MemoryBlock* BlockAllocator::findBlock_R(MemoryBlock* p_Block,size_t p_Size)
   {
-    MemoryBlock* newBlock = 0;
+		MemoryBlock* newBlock = 0;
 
-    //is there enough memory left to satisfy allocation?
-    if(m_MemorySize - m_UsedMemory < p_Size + sizeof(MemoryBlock))
-    {
-      return 0; // nope, try defragmentation
-    }
+		//is there enough memory left to satisfy allocation?
+		if(m_MemorySize - m_UsedMemory < p_Size + sizeof(MemoryBlock))
+		{
+			return 0; // nope, try defragmentation
+		}
 
-    //can the passed block maybe satisfy the allocation?
-    if(p_Block && !p_Block->Used && p_Block->Size >= p_Size)
-    {
-      //yep, mark block as used and return it.
-      p_Block->Used = true;
-      return p_Block;
-    }
+		//can the passed block maybe satisfy the allocation?
+		if(p_Block && !p_Block->Used && p_Block->Size >= p_Size)
+		{
+			//yep, mark block as used and return it.
+			p_Block->Used = true;
+			return p_Block;
+		}
 
-    //if current block can not satisfy allocation,
-    //maybe its neighbor can?
-    if(p_Block && p_Block->Next)
-    {
-      return findBlock_R(p_Block->Next, p_Size);
-    }
+		//if current block can not satisfy allocation,
+		//maybe its neighbor can?
+		if(p_Block && p_Block->Next)
+		{
+			return findBlock_R(p_Block->Next, p_Size);
+		}
 
-    // if no block has been passed in or the block has no neighbor,
-    // create new block
-    if(!p_Block)
-    {
-      newBlock = (MemoryBlock*)m_Memory;
-    }
-    else if(!p_Block->Next)
-    {
-      //Check if we're at the memory boundary
-      ptrdiff_t newBlockEndPos = (ptrdiff_t)(p_Block->Memory + p_Size + sizeof(MemoryBlock));
-      ptrdiff_t memoryEndPos = (ptrdiff_t)m_MemoryEndAddress;
+		// if no block has been passed in or the block has no neighbor,
+		// create new block
+		if(!p_Block)
+		{
+			newBlock = (MemoryBlock*)m_Memory;
+		}
+		else if(!p_Block->Next)
+		{
+			//Check if we're at the memory boundary
+			ptrdiff_t newBlockEndPos = (ptrdiff_t)(p_Block->Memory + p_Size + sizeof(MemoryBlock));
+			ptrdiff_t memoryEndPos = (ptrdiff_t)m_MemoryEndAddress;
 
-      if(newBlockEndPos >= memoryEndPos)
-      {
-        //yep, we are at the memory boundary - try defragmentation
-        return 0;
-      }
+			if(newBlockEndPos >= memoryEndPos)
+			{
+			//yep, we are at the memory boundary - try defragmentation
+			return 0;
+			}
 
-      newBlock = (MemoryBlock*)(p_Block->Memory + p_Block->Size);
-      p_Block->Next = newBlock;
-    }
+			newBlock = (MemoryBlock*)(p_Block->Memory + p_Block->Size);
+			p_Block->Next = newBlock;
+		}
 
-    newBlock->Memory = (byte*)newBlock;
-    newBlock->Memory += sizeof(MemoryBlock);
-    newBlock->Size = p_Size;
-    newBlock->Next = 0;
-    newBlock->Used = true;
+		newBlock->Memory = (byte*)newBlock;
+		newBlock->Memory += sizeof(MemoryBlock);
+		newBlock->Size = p_Size;
+		newBlock->Next = 0;
+		newBlock->Used = true;
 
-    // is this the first allocation?
-    if(!m_First)
-    {
-      m_First = newBlock;
-    }
+		// is this the first allocation?
+		if(!m_First)
+		{
+			m_First = newBlock;
+		}
 
-    // add size of memory block to used memory size as 
-    // we use that much memory additionally per allocation
-    m_UsedMemory += sizeof(MemoryBlock);
+		// add size of memory block to used memory size as 
+		// we use that much memory additionally per allocation
+		m_UsedMemory += sizeof(MemoryBlock);
 
-    return newBlock;
+		return newBlock;
 	}
 	/*********************************************************************************/
 	void BlockAllocator::dealloc(void* p_Pointer,size_t p_Size)
@@ -141,18 +141,18 @@ namespace K15_Engine { namespace Core {
 		{
 			if(!p_Block->Used && !p_Block->Next->Used)
 			{
-        if((p_Block->Size + p_Block->Next->Size) >= p_Size)
-        {
-				  K15_LOG_DEBUG("Merging block %p(size %u) and block %p(size %u) from BlockAllocator \"%s\"...",
-					  p_Block,p_Block->Size,p_Block->Next,p_Block->Next->Size,m_Name.c_str());
+				if((p_Block->Size + p_Block->Next->Size) >= p_Size)
+				{
+						  K15_LOG_DEBUG("Merging block %p(size %u) and block %p(size %u) from BlockAllocator \"%s\"...",
+							  p_Block,p_Block->Size,p_Block->Next,p_Block->Next->Size,m_Name.c_str());
 
-				  p_Block->Size += p_Block->Next->Size + sizeof(MemoryBlock); //memblock is part of memory so we add the size of the block to get the 'real' size.
-				  p_Block->Next = p_Block->Next->Next;
+						  p_Block->Size += p_Block->Next->Size + sizeof(MemoryBlock); //memblock is part of memory so we add the size of the block to get the 'real' size.
+						  p_Block->Next = p_Block->Next->Next;
 
-          // keep track of how much memory is used
-          m_UsedMemory -= sizeof(MemoryBlock);
-          break;
-        }
+				  // keep track of how much memory is used
+				  m_UsedMemory -= sizeof(MemoryBlock);
+				  break;
+				}
 			}
 
 			p_Block = p_Block->Next;
