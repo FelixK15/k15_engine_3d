4836b571086ad83f45812733184f737fafa08db2
Felix Klinge <f.klinge15@gmail.com>
2015-08-10 23:43:54 +0200

    - removed [core] deprecated stuff

--- core/rendering/src/OpenGL/GL3/K15_RenderGL3Frame.cpp
+++
@@ -1,9 +0,0 @@
- /*********************************************************************************/
-inline uint8 K15_GLClearScreen(K15_RenderContext* p_RenderContext)
-{
-	kglSwapBuffers((K15_GLRenderContext*)p_RenderContext->userData);
-	kglClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
-	return K15_SUCCESS;
-}
-/*********************************************************************************/
--- deprecated/engine/cmake/engine_shader_src.cmake
+++
@@ -1,9 +0,0 @@
-set(VERTEX_SHADER_SRC 
-".././shaders/default.vert"
-".././shaders/font.vert"
-)
-
-set(FRAGMENT_SHADER_SRC
-".././shaders/default.frag"
-".././shaders/font.frag"
-)
